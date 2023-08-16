a0224f3889715effacfcd4563886a4c7a1e61f49
cantrellr-gov <charles.cantell@usdoj.gov>
2023-08-16 09:31:15 -0400

    initial commit

--- outputs.tf
+++ outputs.tf
@@ -2,5 +2,5 @@ output "efa_urlallowlist_id" {
     value = zia_url_categories.efa_urlcategories.id
 }
 output "efa_urlallowlist_name" {
-    value = zia_url_categories.efa_urlcategories.name
+    value = zia_url_categories.efa_urlcategories.configured_name
 }
